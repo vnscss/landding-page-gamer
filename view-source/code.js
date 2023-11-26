c7d2f356a169fbd7e18d1b90850e8b68b92f3f9a
Vinícius Alves Pires <147334176+vnscss@users.noreply.github.com>
2023-11-25 22:38:51 -0300

    Initial commit

---
+++ view-source/code.js
@@ -0,0 +1,9 @@
+function btnHtml() {
+    document.getElementById("iframe").src = "view-source/view-html.html";
+}
+  function btnCss() {
+    document.getElementById("iframe").src = "view-source/view-css.html";
+}
+  function btnJs() {
+    document.getElementById("iframe").src = "view-source/view-js.html";
+}
